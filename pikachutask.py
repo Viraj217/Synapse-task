63918a498098fc031040861a9dc7fca025c11802
Viraj Shah <virajshah177@gmail.com>
2025-08-28 13:02:37 +0530

    initial commit

---
+++ pikachutask.py
@@ -0,0 +1,15 @@
+Pokedex = {
+"Pikachu": ('Electric'),
+            "Charizard": ("Fire", "Flying"),
+"Lapras": ("Water", "Ice"),
+"Machamp": ("Fighting",),
+"Mewtwo": ("Psychic", "Fighting"),
+"Hoopa": ("Psychic", "Ghost", "Dark"),
+"Lugia": ("Psychic", "Flying", "Water"),
+"Squirtle": ("Water",),
+"Gengar": ("Ghost", "Poison"),
+"Onix": ("Rock", "Ground"),
+}
+
+
+def pika()
